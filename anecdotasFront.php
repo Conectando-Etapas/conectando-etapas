a76ee30b362fb67e0b6319d816a43f45cd31c0fd
Dalviux9th <enzod3339@gmail.com>
2024-03-17 21:37:12 -0300

    eliminada antigua carpeta www

--- anecdotasFront.php
+++ anecdotasFront.php
@@ -18,8 +18,8 @@ $anec_fetch = mysqli_query($link, "SELECT * FROM anecdota");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Cantarell" />
 
-    <link rel="stylesheet" href="http://localhost/www/sources/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://localhost/www/sources/anecdotas.css">
+    <link rel="stylesheet" href="./sources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./sources/anecdotas.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
--- www/includes/db_con.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "root", "", "conectando_etapas");
-
-    if (!$link)
-        echo "404. Something went wrong.";
-    
-?>
