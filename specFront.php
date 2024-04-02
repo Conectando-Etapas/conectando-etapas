3dbf0cf5afe204a3734eda0a102a1ef9963037b2
Dalviux9th <enzod3339@gmail.com>
2024-04-01 22:40:44 -0300

    specFront lista

--- specFront.php
+++ specFront.php
@@ -2,12 +2,12 @@
 include_once 'includes/db_con.php';
 
 switch ($_GET["orientacion"]) {
-    case 'programacion':
     case 'mmo':
-    case 'automotores':
+    case 'programacion':
     case 'quimica':         
     case 'electromecanica':
     case 'informatica':
+    case 'automotores':
 
         $id = $_GET["orientacion"];
         $res = mysqli_query($link, "SELECT * FROM `orientacion` WHERE orientacion = '$id' ");
@@ -28,12 +28,12 @@ switch ($_GET["orientacion"]) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Cantarell" />
     
-    <link rel="stylesheet" href="http://localhost/www/sources/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://localhost/www/sources/main_config.css">
-    <link rel="stylesheet" href="http://localhost/www/sources/especializacion.css">
+    <link rel="stylesheet" href="./sources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./sources/main_config.css">
+    <link rel="stylesheet" href="./sources/especializacion.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <link rel="stylesheet" href="http://localhost/www/sources/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./sources/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
     
     <script src="https://kit.fontawesome.com/e7401517de.js" crossorigin="anonymous"></script>
@@ -71,6 +71,7 @@ switch ($_GET["orientacion"]) {
                 </div>
             </div>
         </nav>
+        <div class="sub-nav col-12"></div>
     </header>
     
     
@@ -163,11 +164,11 @@ switch ($_GET["orientacion"]) {
 
     <script src="https://kit.fontawesome.com/5b34b9778f.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script src="http://localhost/www/sources/js/bootstrap.min.js"></script>
+    <script src="./sources/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="http://localhost/www/sources/js/bootstrap.bundle.js"></script>
+    <script src="./sources/js/bootstrap.bundle.js"></script>
     
 </body>
 </html>
